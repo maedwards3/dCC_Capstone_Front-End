81cc7bd294840e96bc407c09477036ea25d7f9e9
maedwards3 <michael.edwards.4108@gmail.com>
2021-07-30 11:25:01 -0400

    Added Google Maps Embedded API.

--- random_restaurant_selector/src/Components/ReturnedRestaurants/returnedRestaurants.jsx
+++ random_restaurant_selector/src/Components/ReturnedRestaurants/returnedRestaurants.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 
-export default function ReturnedRestaurants() {
+export default function ReturnedRestaurants(props) {
 
-    
     
     return (
-        <div>
-
-        </div>
+        <React.Fragment>
+            <h1> Google Map</h1>
+            <iframe 
+                width="450"
+                height="250"
+                frameBorder="0"
+                src={`https://www.google.com/maps/embed/v1/view?zoom=10&center=${props.lat}%2C-78.6382&key=AIzaSyDNVtd9ME7RAjYylRcq9J7uFIeriXQbUq8`}
+            ></iframe>
+        </React.Fragment>
     );
 }
