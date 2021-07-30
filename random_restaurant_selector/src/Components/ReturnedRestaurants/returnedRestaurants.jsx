c98a3a073107df70ed7316643de6f5d5b8985106
maedwards3 <michael.edwards.4108@gmail.com>
2021-07-30 14:32:26 -0400

    Added buttons to re-search and start filter process from beginning.

--- random_restaurant_selector/src/Components/ReturnedRestaurants/returnedRestaurants.jsx
+++ random_restaurant_selector/src/Components/ReturnedRestaurants/returnedRestaurants.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Typography } from '@material-ui/core';
+import { Button } from '@material-ui/core';
+import { Link } from 'react-router-dom';
 
 export default function ReturnedRestaurants(props) {
 
-    
     return (
         <React.Fragment>
             <h1> Google Map</h1>
@@ -12,6 +14,27 @@ export default function ReturnedRestaurants(props) {
                 frameBorder="0"
                 src={`https://www.google.com/maps/embed/v1/view?zoom=10&center=${props.lat}%2C-78.6382&key=AIzaSyDNVtd9ME7RAjYylRcq9J7uFIeriXQbUq8`}
             ></iframe>
+            <div>
+                <div>
+                    <Typography>
+                        Don't like your result? 
+                    </Typography>
+                    <Button
+                        variant="outlined"
+                        color="Primary"
+                    >
+                        Search again!
+                    </Button>
+                    <Button
+                        variant="outlined"
+                        color="Primary"
+                    >
+                        <Link to="filter">
+                            Start from scratch
+                        </Link>
+                    </Button>
+                </div>
+            </div>
         </React.Fragment>
     );
 }
