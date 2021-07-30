865a5441fc10187e4e5284844dd1d06e20baddb2
maedwards3 <michael.edwards.4108@gmail.com>
2021-07-30 11:26:31 -0400

    Added css file for styling. Added button with link to filter items.

--- random_restaurant_selector/src/Components/Dashboard/dashboard.jsx
+++ random_restaurant_selector/src/Components/Dashboard/dashboard.jsx
@@ -1,5 +1,23 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import './dashboard.css';
 
 export default function Dashboard () {
-    return <></>;
+    return (
+        <div>
+            <div >
+                <Button 
+                    variant="outlined"
+                    color="secondary"
+                    className="filter-button"
+                >
+                    <Link to="filter">
+                        Find a random restaurant!
+                    </Link>
+                </Button>
+            </div>
+
+        </div>    
+    );
 }
