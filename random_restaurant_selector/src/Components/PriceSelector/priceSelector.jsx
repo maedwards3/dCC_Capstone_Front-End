627cde7604f257ab96be62b0cf9e750b73526abd
maedwards3 <michael.edwards.4108@gmail.com>
2021-08-01 22:05:49 -0400

    Refactored to pass user input state to App.js for api call. Added buttons for testing. Will clean up once app is fully functional.

--- random_restaurant_selector/src/Components/PriceSelector/priceSelector.jsx
+++ random_restaurant_selector/src/Components/PriceSelector/priceSelector.jsx
@@ -1,76 +1,42 @@
 import React, { useState } from 'react';
 import AttachMoneyIcon from '@material-ui/icons/AttachMoney';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControl from '@material-ui/core/FormControl';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormGroup from '@material-ui/core/FormGroup';
-import Checkbox from '@material-ui/core/Checkbox';
+import FormLabel from '@material-ui/core/FormLabel';
 import './priceSelector.css';
 import { Button } from '@material-ui/core'
 import { Link } from 'react-router-dom';
 
 
-const PriceSelector = () => {
-    const[selected, setSelected] = useState(false);
+const PriceSelector = (props) => {
+    const[selected, setSelected] = useState('');
+
+    const handleChange = (event) => {
+        const { value } = event.target;
+        if (value) {
+            setSelected(value);
+            console.log(selected);
+        }
+    }
 
     return(
         <div className="price-selector-box">
             <div>
-                <FormControl>
-                    <FormGroup>
-                    <FormControlLabel
-                        value="end"
-                        control={<Checkbox color="primary" />}
-                        label=""
-                        labelPlacement="end"
-                    />
-                    </FormGroup>
-                </FormControl>
-                <AttachMoneyIcon/>
+                <FormLabel>
+                    Select your max price range
+                </FormLabel>
             </div>
             <div>
                 <FormControl>
-                    <FormGroup>
-                        <FormControlLabel
-                            value="end"
-                            control={<Checkbox color="primary" />}
-                            label=""
-                            labelPlacement="end"
-                        />
-                    </FormGroup>
+                    <RadioGroup aria-label="gender" name="gender1" value={selected} onChange={handleChange}>
+                        <FormControlLabel value="one" control={<Radio />} label={<AttachMoneyIcon/>} />
+                        <FormControlLabel value="two" control={<Radio />} label={<><AttachMoneyIcon/><AttachMoneyIcon/></>} />
+                        <FormControlLabel value="three" control={<Radio />} label={<><AttachMoneyIcon/><AttachMoneyIcon/><AttachMoneyIcon/></>} />
+                        <FormControlLabel value="four" control={<Radio />} label={<><AttachMoneyIcon/><AttachMoneyIcon/><AttachMoneyIcon/><AttachMoneyIcon/></>} />
+                    </RadioGroup>
                 </FormControl>
-                <AttachMoneyIcon/>
-                <AttachMoneyIcon/>
-            </div>
-            <div>
-            <FormControl>
-                <FormGroup>
-                    <FormControlLabel
-                        value="end"
-                        control={<Checkbox color="primary" />}
-                        label=""
-                        labelPlacement="end"
-                    />
-                </FormGroup>
-            </FormControl>
-            <AttachMoneyIcon/>
-            <AttachMoneyIcon/>
-            <AttachMoneyIcon/>
-            </div>
-            <div>
-            <FormControl>
-                <FormGroup>
-                    <FormControlLabel
-                        value="end"
-                        control={<Checkbox color="primary" />}
-                        label=""
-                        labelPlacement="end"
-                    />
-                </FormGroup>
-            </FormControl>
-            <AttachMoneyIcon/>
-            <AttachMoneyIcon/>
-            <AttachMoneyIcon/>
-            <AttachMoneyIcon/>
             </div>
             <div>
                 <Button 
@@ -85,10 +51,20 @@ const PriceSelector = () => {
             <div>
                 <Button 
                     variant="outlined"
-                    color="Primary">
+                    color="Primary"
+                    onclick={() => props.setFinalizePrice(selected)}
+                >
                     <Link to="/filter3">
                         Select distance
                     </Link>
+                </Button>
+            </div>
+            <div>
+                <Button >
+                    Click me
+                </Button>
+                <Button onClick={() => console.log(selected)}>
+                    Click me next
                 </Button>
             </div>
         </div>
