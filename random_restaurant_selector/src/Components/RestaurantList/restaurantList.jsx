40d0d29f9b7c9eb46c5bcaa6477e01ac13c81f71
maedwards3 <michael.edwards.4108@gmail.com>
2021-08-01 22:08:11 -0400

    Created RestaurantList component--requires logic.

---
+++ random_restaurant_selector/src/Components/RestaurantList/restaurantList.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './restaurantList.css';
+
+const RestaurantList = () => {
+
+
+    return (
+        // map over returned results from Google Places Find Place search -- limit 20 results if possible
+        <div className="restaurant-list-container">
+
+        </div>
+    );
+}
+
+export default RestaurantList;
