efd0b1dd7c1bbf3c0b1e94a6f34880fc8d45b937
maedwards3 <michael.edwards.4108@gmail.com>
2021-08-01 21:56:29 -0400

    Refactored to send user input state to app for api call. Added buttons for testing.

--- random_restaurant_selector/src/Components/CuisineTypes/cuisineTypes.jsx
+++ random_restaurant_selector/src/Components/CuisineTypes/cuisineTypes.jsx
@@ -9,142 +9,156 @@ import { Button } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 
 
-export default function CuisineTypes() {
+export default function CuisineTypes(props) {
     const [state, setState] = useState({
-        American: true,
-        Arab: true,
-        BBQ: true,
-        Breakfast: true,
-        Cajun: true,
-        Caribbean: true,
-        Chinese: true,
-        Filipino: true,
-        French: true,
-        German: true,
-        Greek: true,
-        Indian: true,
-        Italian: true,
-        Japanese: true,
-        Korean: true,
-        Mediterranean: true,
-        Mexican: true,
-        Pizza: true,
-        Thai: true,
-        Turkish: true,
-        Vegetarian: true,
-        Vietnamese: true,
+        american: false,
+        arab: false,
+        bbq: false,
+        breakfast: false,
+        cajun: false,
+        caribbean: false,
+        chinese: false,
+        filipino: false,
+        french: false,
+        german: false,
+        greek: false,
+        indian: false,
+        italian: false,
+        japanese: false,
+        korean: false,
+        mediterranean: false,
+        mexican: false,
+        pizza: false,
+        thai: false,
+        turkish: false,
+        vegetarian: false,
+        vietnamese: false,
     });
 
     const handleChange = (event) => {
         setState({ ...state, [event.target.name]: event.target.checked });
     };
 
+    const filterCuisines = () => {
+        const options = Object.keys(state);
+        return options.filter(o => state[o]);
+    };
+
     return (
         <div className="cuisine-selectors">
-            <FormControl component="fieldset" >
-                <FormLabel component="legend">Select your cuisine(s)</FormLabel>
-                <FormGroup>
-                    <FormControlLabel
-                        control={<Switch checked={state.American} onChange={handleChange} name="American" />}
-                        label="American"
-                    />
-                    <FormControlLabel
-                        control={<Switch checked={state.Arab} onChange={handleChange} name="Arab" />}
-                        label="Arab"
-                    />
-                    <FormControlLabel
-                        control={<Switch checked={state.BBQ} onChange={handleChange} name="BBQ" />}
-                        label="BBQ"
-                    />
-                    <FormControlLabel
-                        control={<Switch checked={state.Breakfast} onChange={handleChange} name="Breakfast" />}
-                        label="Breakfast"
-                    />
-                    <FormControlLabel
-                        control={<Switch checked={state.Cajun} onChange={handleChange} name="Cajun" />}
-                        label="Cajun"
-                    />
-                    <FormControlLabel
-                        control={<Switch checked={state.Caribbean} onChange={handleChange} name="Caribbean" />}
-                        label="Caribbean"
-                    />
-                    <FormControlLabel
-                        control={<Switch checked={state.Chinese} onChange={handleChange} name="Chinese" />}
-                        label="Chinese"
-                    />
-                    <FormControlLabel
-                        control={<Switch checked={state.Filipino} onChange={handleChange} name="Filipino" />}
-                        label="Filipino"
-                    />
-                    <FormControlLabel
-                        control={<Switch checked={state.French} onChange={handleChange} name="French" />}
-                        label="French"
-                    />
-                    <FormControlLabel
-                        control={<Switch checked={state.German} onChange={handleChange} name="German" />}
-                        label="German"
-                    />
-                    <FormControlLabel
-                        control={<Switch checked={state.Greek} onChange={handleChange} name="Greek" />}
-                        label="Greek"
-                    />
-                    <FormControlLabel
-                        control={<Switch checked={state.Indian} onChange={handleChange} name="Indian" />}
-                        label="Indian"
-                    />
-                    <FormControlLabel
-                        control={<Switch checked={state.Italian} onChange={handleChange} name="Italian" />}
-                        label="Italian"
-                    />
-                    <FormControlLabel
-                        control={<Switch checked={state.Japanese} onChange={handleChange} name="Japanese" />}
-                        label="Japanese"
-                    />
-                    <FormControlLabel
-                        control={<Switch checked={state.Korean} onChange={handleChange} name="Korean" />}
-                        label="Korean"
-                    />
-                    <FormControlLabel
-                        control={<Switch checked={state.Mediterranean} onChange={handleChange} name="Mediterranean" />}
-                        label="Mediterranean"
-                    />
-                    <FormControlLabel
-                        control={<Switch checked={state.Mexican} onChange={handleChange} name="Mexican" />}
-                        label="Mexican"
-                    />
-                    <FormControlLabel
-                        control={<Switch checked={state.Pizza} onChange={handleChange} name="Pizza" />}
-                        label="Pizza"
-                    />
-                    <FormControlLabel
-                        control={<Switch checked={state.Thai} onChange={handleChange} name="Thai" />}
-                        label="Thai"
-                    />
-                    <FormControlLabel
-                        control={<Switch checked={state.Turkish} onChange={handleChange} name="Turkish" />}
-                        label="Turkish"
-                    />
-                    <FormControlLabel
-                        control={<Switch checked={state.Vegetarian} onChange={handleChange} name="Vegetarian" />}
-                        label="Vegetarian"
-                    />
-                    <FormControlLabel
-                        control={<Switch checked={state.Vietnamese} onChange={handleChange} name="Vietnamese" />}
-                        label="Vietnamese"
-                        color="Primary"
-                    />
-                </FormGroup>
-            </FormControl>
-
-            <Button 
-                variant="outlined"
-                color="Primary"
-            >
-                <Link to="/filter2">
-                    Select pricing
-                </Link>
+            <div>
+                <FormControl component="fieldset" >
+                    <FormLabel component="legend">Select your cuisine(s)</FormLabel>
+                    <FormGroup>
+                        <FormControlLabel
+                            control={<Switch checked={state.American} onChange={handleChange} name="american" />}
+                            label="American"
+                        />
+                        <FormControlLabel
+                            control={<Switch checked={state.Arab} onChange={handleChange} name="arab" />}
+                            label="Arab"
+                        />
+                        <FormControlLabel
+                            control={<Switch checked={state.BBQ} onChange={handleChange} name="bbq" />}
+                            label="BBQ"
+                        />
+                        <FormControlLabel
+                            control={<Switch checked={state.Breakfast} onChange={handleChange} name="breakfast" />}
+                            label="Breakfast"
+                        />
+                        <FormControlLabel
+                            control={<Switch checked={state.Cajun} onChange={handleChange} name="cajun" />}
+                            label="Cajun"
+                        />
+                        <FormControlLabel
+                            control={<Switch checked={state.Caribbean} onChange={handleChange} name="caribbean" />}
+                            label="Caribbean"
+                        />
+                        <FormControlLabel
+                            control={<Switch checked={state.Chinese} onChange={handleChange} name="chinese" />}
+                            label="Chinese"
+                        />
+                        <FormControlLabel
+                            control={<Switch checked={state.Filipino} onChange={handleChange} name="filipino" />}
+                            label="Filipino"
+                        />
+                        <FormControlLabel
+                            control={<Switch checked={state.French} onChange={handleChange} name="french" />}
+                            label="French"
+                        />
+                        <FormControlLabel
+                            control={<Switch checked={state.German} onChange={handleChange} name="german" />}
+                            label="German"
+                        />
+                        <FormControlLabel
+                            control={<Switch checked={state.Greek} onChange={handleChange} name="greek" />}
+                            label="Greek"
+                        />
+                        <FormControlLabel
+                            control={<Switch checked={state.Indian} onChange={handleChange} name="indian" />}
+                            label="Indian"
+                        />
+                        <FormControlLabel
+                            control={<Switch checked={state.Italian} onChange={handleChange} name="italian" />}
+                            label="Italian"
+                        />
+                        <FormControlLabel
+                            control={<Switch checked={state.Japanese} onChange={handleChange} name="japanese" />}
+                            label="Japanese"
+                        />
+                        <FormControlLabel
+                            control={<Switch checked={state.Korean} onChange={handleChange} name="korean" />}
+                            label="Korean"
+                        />
+                        <FormControlLabel
+                            control={<Switch checked={state.Mediterranean} onChange={handleChange} name="mediterranean" />}
+                            label="Mediterranean"
+                        />
+                        <FormControlLabel
+                            control={<Switch checked={state.Mexican} onChange={handleChange} name="mexican" />}
+                            label="Mexican"
+                        />
+                        <FormControlLabel
+                            control={<Switch checked={state.Pizza} onChange={handleChange} name="pizza" />}
+                            label="Pizza"
+                        />
+                        <FormControlLabel
+                            control={<Switch checked={state.Thai} onChange={handleChange} name="thai" />}
+                            label="Thai"
+                        />
+                        <FormControlLabel
+                            control={<Switch checked={state.Turkish} onChange={handleChange} name="turkish" />}
+                            label="Turkish"
+                        />
+                        <FormControlLabel
+                            control={<Switch checked={state.Vegetarian} onChange={handleChange} name="vegetarian" />}
+                            label="Vegetarian"
+                        />
+                        <FormControlLabel
+                            control={<Switch checked={state.Vietnamese} onChange={handleChange} name="vietnamese" />}
+                            label="Vietnamese"
+                        />
+                    </FormGroup>
+                </FormControl>
+            </div>
+            <div>
+                <Button 
+                    variant="outlined"
+                    color="Primary"
+                    onClick={() => props.setFinalizeCuisine(filterCuisines())}
+                >
+                    <Link to="/filter2">
+                        Select pricing
+                    </Link>
+                </Button>
+            </div>
+            <Button onClick={() => props.setFinalizeCuisine(state)}>
+                Click me
             </Button>
 
+            <Button onClick={() => console.log(state)}>
+                Click me next
+            </Button>
         </div>
     );
 }
