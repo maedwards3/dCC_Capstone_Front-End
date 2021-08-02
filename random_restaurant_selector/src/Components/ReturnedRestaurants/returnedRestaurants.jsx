7bbbcf0b94a95e8d3d604200f585553d3f6c9041
maedwards3 <michael.edwards.4108@gmail.com>
2021-08-01 22:09:37 -0400

    Added buttons for redirect--requires logic on other components and api to render to be functional.

--- random_restaurant_selector/src/Components/ReturnedRestaurants/returnedRestaurants.jsx
+++ random_restaurant_selector/src/Components/ReturnedRestaurants/returnedRestaurants.jsx
@@ -2,17 +2,19 @@ import React from 'react';
 import { Typography } from '@material-ui/core';
 import { Button } from '@material-ui/core';
 import { Link } from 'react-router-dom';
+import './returnedRestaurants.css';
 
 export default function ReturnedRestaurants(props) {
+    const origin = props.origin;
 
     return (
-        <React.Fragment>
+        <React.Fragment className="returned-results-container">
             <h1> Google Map</h1>
             <iframe 
-                width="450"
-                height="250"
+                width="600"
+                height="450"
                 frameBorder="0"
-                src={`https://www.google.com/maps/embed/v1/view?zoom=10&center=${props.lat}%2C-78.6382&key=AIzaSyDNVtd9ME7RAjYylRcq9J7uFIeriXQbUq8`}
+                // src={`https://www.google.com/maps/embed/v1/view?zoom=10&center=35.7796,78.6382%2C-78.6382&key=${process.env.GOOGLE_MAPS_EMBEDDED_KEY}`}
             ></iframe>
             <div>
                 <div>
@@ -29,10 +31,35 @@ export default function ReturnedRestaurants(props) {
                         variant="outlined"
                         color="Primary"
                     >
-                        <Link to="filter">
+                        <Link to="/filter">
                             Start from scratch
                         </Link>
                     </Button>
+                    <Button
+                        variant="outlined"
+                        color="Primary"
+                    >
+                        <Link to="/returnedList">
+                            Select from a list meeting your criteria
+                        </Link>
+                    </Button>
+                </div>
+                <div>
+                    <div>
+                        <Typography>
+                            Happy with your result? Click below to confirm!
+                        </Typography>
+                    </div>
+                    <div>
+                        <Button
+                            variant="outlined"
+                            color="Primary"
+                        >
+                            <Link to="/confirm">
+                                Confirm
+                            </Link>
+                        </Button>
+                    </div>
                 </div>
             </div>
         </React.Fragment>
